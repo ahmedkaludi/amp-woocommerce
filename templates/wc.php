8ecba066b8ce02cb4a215a9f1dc5edbdd6e85385
Mohammed Kaludi <mohammed@magazine3.com>
2017-06-17 03:42:26 +0530

    Fixed Viewport #17
    Fixed Viewport #17

--- templates/wc.php
+++ templates/wc.php
@@ -2,7 +2,6 @@
 <html amp <?php echo AMP_HTML_Utils::build_attributes_string( $this->get( 'html_tag_attributes' ) ); ?>>
 <head>
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no">
 	<?php do_action( 'amp_post_template_head', $this ); ?>
 	<style amp-custom>
 		<?php $this->load_parts( array( 'style' ) ); ?>
