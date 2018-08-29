f5300053c17edc25e72c82ddc1743c5d390f66aa
Marqas <marqas36@gmail.com>
2018-08-29 13:26:04 +0530

    Can we get it with proper vars

--- scripts/deploy3.sh
+++ scripts/deploy3.sh
@@ -74,12 +74,12 @@ svn st | grep '^?' | sed -e 's/\?[ ]*/svn add -q /g' | sh
 echo " Add and remove done"
 
 # If tag number and credentials are provided, commit to trunk.
-if [[ $TRAVIS_TAG && $SVN_USER && $SVN_PASS ]]; then
+if [[ $TRAVIS_TAG && $WP_ORG_USERNAME && $WP_ORG_PASSWORD ]]; then
 	if [[ ! -d tags/$TRAVIS_TAG ]]; then
 		echo "Commit to $SVN_REPO."
-		svn commit -m "commit version $TRAVIS_TAG" --username $SVN_USER --password $SVN_PASS --non-interactive 2>/dev/null
+		svn commit -m "commit version $TRAVIS_TAG" --username $WP_ORG_USERNAME --password $WP_ORG_PASSWORD --non-interactive 2>/dev/null
 		echo "Take snapshot of $TRAVIS_TAG"
-		svn copy $SVN_REPO/trunk $SVN_REPO/tags/$TRAVIS_TAG -m "Take snapshot of $TRAVIS_TAG" --username $SVN_USER --password $SVN_PASS --non-interactive 2>/dev/null
+		svn copy $SVN_REPO/trunk $SVN_REPO/tags/$TRAVIS_TAG -m "Take snapshot of $TRAVIS_TAG" --username $WP_ORG_USERNAME --password $WP_ORG_PASSWORD --non-interactive 2>/dev/null
 	else
 		echo "tags/$TRAVIS_TAG already exists."
 	fi
