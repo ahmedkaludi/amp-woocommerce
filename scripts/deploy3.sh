99c922a8dc520f2a29afeb7aabd6e1ebdd09130e
Marqas <marqas36@gmail.com>
2018-08-29 11:40:37 +0530

    Lets test with this script

---
+++ scripts/deploy3.sh
@@ -0,0 +1,23 @@
+set -e
+
+if [[ "false" != "$TRAVIS_PULL_REQUEST" ]]; then
+	echo "Not deploying pull requests."
+	exit
+fi
+
+if [[ ! $WP_PULUGIN_DEPLOY ]]; then
+	echo "Not deploying."
+	exit
+fi
+
+if [[ ! $SVN_REPO ]]; then
+	echo "SVN repo is not specified."
+	exit
+fi
+
+# Untrailing slash of SVN_REPO path
+SVN_REPO=`echo $SVN_REPO | sed -e "s/\/$//"`
+# Git repository
+GH_REF=https://github.com/${TRAVIS_REPO_SLUG}.git
+
+echo "Starting deploy..."
