071f244169c050282617ae854d3cb8e2424d427b
Marqas <marqas36@gmail.com>
2018-08-29 18:51:16 +0530

    is it deleting the files?

--- scripts/deploy3.sh
+++ scripts/deploy3.sh
@@ -34,72 +34,72 @@ echo "Delete the beta directory first $SVN_REPO/tags/$TRAVIS_TAG/"
 rm -fr $SVN_REPO/tags/$TRAVIS_TAG/
 echo "Deleting of beta done"
 
-echo "Delete the trunk directory first $SVN_REPO/tags/$TRAVIS_TAG/"
+echo "Delete the trunk directory first $SVN_REPO/trunk/"
 rm -fr $SVN_REPO/trunk/
 echo "Deleting of trunk done"
 
-mkdir build
+# mkdir build
 
-cd build
-BASE_DIR=$(pwd)
+# cd build
+# BASE_DIR=$(pwd)
 
-echo "$BASE_DIR"
+# echo "$BASE_DIR"
 
-echo "Checking out trunk from $SVN_REPO ..."
-svn co -q $SVN_REPO/trunk
+# echo "Checking out trunk from $SVN_REPO ..."
+# svn co -q $SVN_REPO/trunk
 
-echo "Getting clone from $GH_REF to $SVN_REPO ..."
-git clone -q $GH_REF ./git
-echo "cloning done"
-cd ./git
-echo "in the directory"
+# echo "Getting clone from $GH_REF to $SVN_REPO ..."
+# git clone -q $GH_REF ./git
+# echo "cloning done"
+# cd ./git
+# echo "in the directory"
 
-cd $BASE_DIR
-echo "in $BASE_DIR"
-echo "Syncing git repository to svn"
-rsync -a --exclude=".svn" --checksum --delete ./git/ ./trunk/
-rm -fr ./git
-echo "Syncing done"
+# cd $BASE_DIR
+# echo "in $BASE_DIR"
+# echo "Syncing git repository to svn"
+# rsync -a --exclude=".svn" --checksum --delete ./git/ ./trunk/
+# rm -fr ./git
+# echo "Syncing done"
 
-cd ./trunk
+# cd ./trunk
 
-if [ -e ".distignore" ]; then
-	echo "svn propset form .distignore"
-	svn propset -q -R svn:ignore -F .distignore .
+# if [ -e ".distignore" ]; then
+# 	echo "svn propset form .distignore"
+# 	svn propset -q -R svn:ignore -F .distignore .
 
-else
-	if [ -e ".svnignore" ]; then
-		echo "svn propset"
-		svn propset -q -R svn:ignore -F .svnignore .
-	fi
-fi
-echo "Ignoring GitHub specific files"
-svn propset svn:ignore "README.md
-Thumbs.db
-.github/*
-.git
-.gitattributes
-.gitignore" "$SVNPATH/trunk/"
-echo "Run svn add"
-svn st | grep '^!' | sed -e 's/\![ ]*/svn del -q /g' | sh
-echo "Run svn del"
-svn st | grep '^?' | sed -e 's/\?[ ]*/svn add -q /g' | sh
+# else
+# 	if [ -e ".svnignore" ]; then
+# 		echo "svn propset"
+# 		svn propset -q -R svn:ignore -F .svnignore .
+# 	fi
+# fi
+# echo "Ignoring GitHub specific files"
+# svn propset svn:ignore "README.md
+# Thumbs.db
+# .github/*
+# .git
+# .gitattributes
+# .gitignore" "$SVNPATH/trunk/"
+# echo "Run svn add"
+# svn st | grep '^!' | sed -e 's/\![ ]*/svn del -q /g' | sh
+# echo "Run svn del"
+# svn st | grep '^?' | sed -e 's/\?[ ]*/svn add -q /g' | sh
 
-echo " Add and remove done"
+# echo " Add and remove done"
 
-# If tag number and credentials are provided, commit to trunk.
-if [[ $TRAVIS_TAG && $WP_ORG_USERNAME && $WP_ORG_PASSWORD ]]; then
-	if [[ ! -d tags/$TRAVIS_TAG ]]; then
-		echo "Commit to $SVN_REPO."
-		svn commit -m "commit version $TRAVIS_TAG" --username $WP_ORG_USERNAME --password $WP_ORG_PASSWORD --non-interactive 2>/dev/null
-		echo "Take snapshot of $TRAVIS_TAG"
-		svn copy $SVN_REPO/trunk/ $SVN_REPO/tags/$TRAVIS_TAG -m "Take snapshot of $TRAVIS_TAG" --username $WP_ORG_USERNAME --password $WP_ORG_PASSWORD --non-interactive 2>/dev/null
-	else
-		echo "tags/$TRAVIS_TAG already exists."
-	fi
-else
-	echo "Nothing to commit and check \`svn st\`."
-	svn st
-fi
+# # If tag number and credentials are provided, commit to trunk.
+# if [[ $TRAVIS_TAG && $WP_ORG_USERNAME && $WP_ORG_PASSWORD ]]; then
+# 	if [[ ! -d tags/$TRAVIS_TAG ]]; then
+# 		echo "Commit to $SVN_REPO."
+# 		svn commit -m "commit version $TRAVIS_TAG" --username $WP_ORG_USERNAME --password $WP_ORG_PASSWORD --non-interactive 2>/dev/null
+# 		echo "Take snapshot of $TRAVIS_TAG"
+# 		svn copy $SVN_REPO/trunk/ $SVN_REPO/tags/$TRAVIS_TAG -m "Take snapshot of $TRAVIS_TAG" --username $WP_ORG_USERNAME --password $WP_ORG_PASSWORD --non-interactive 2>/dev/null
+# 	else
+# 		echo "tags/$TRAVIS_TAG already exists."
+# 	fi
+# else
+# 	echo "Nothing to commit and check \`svn st\`."
+# 	svn st
+# fi
 
-echo "Everythings done"
+# echo "Everythings done"
