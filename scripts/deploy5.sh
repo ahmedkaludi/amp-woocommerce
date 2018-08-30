d660903a458f808fb2e899c0a3afa7c8eb68791f
Marqas <marqas36@gmail.com>
2018-08-30 17:02:02 +0530

    lets try with temp folder

--- scripts/deploy5.sh
+++ scripts/deploy5.sh
@@ -16,7 +16,7 @@ if [[ ! $SVN_REPO ]]; then
 	echo "SVN repo is not specified."
 	exit
 fi
-TRAVIS_TAG="beta2"
+TRAVIS_TAG="beta3"
 SVN_USER="$WP_ORG_USERNAME"
 SVN_PASS="$WP_ORG_PASSWORD"
 # Untrailing slash of SVN_REPO path
@@ -31,8 +31,8 @@ mkdir build
 cd build
 BASE_DIR=$(pwd)
 
-echo "Checking out trunk from $SVN_REPO ..."
-svn co -q $SVN_REPO/trunk
+echo "Checking out temp from $SVN_REPO ..."
+svn co -q $SVN_REPO/temp
 
 echo "Getting clone from $GH_REF to $SVN_REPO ..."
 git clone -q $GH_REF ./git
@@ -47,10 +47,10 @@ fi
 cd $BASE_DIR
 
 echo "Syncing git repository to svn"
-rsync -a --exclude=".svn" --checksum --delete ./git/ ./trunk/
+rsync -a --exclude=".svn" --checksum --delete ./git/ ./temp/
 rm -fr ./git
 
-cd ./trunk
+cd ./temp
 
 if [ -e ".distignore" ]; then
 	echo "svn propset form .distignore"
@@ -68,13 +68,13 @@ svn st | grep '^!' | sed -e 's/\![ ]*/svn del -q /g' | sh
 echo "Run svn del"
 svn st | grep '^?' | sed -e 's/\?[ ]*/svn add -q /g' | sh
 
-# If tag number and credentials are provided, commit to trunk.
+# If tag number and credentials are provided, commit to temp.
 if [[ $TRAVIS_TAG && $SVN_USER && $SVN_PASS ]]; then
 	if [[ ! -d tags/$TRAVIS_TAG ]]; then
 		echo "Commit to $SVN_REPO."
 		svn commit -m "commit version $TRAVIS_TAG" --username $SVN_USER --password $SVN_PASS --non-interactive 2>/dev/null
 		echo "Take snapshot of $TRAVIS_TAG"
-		svn copy $SVN_REPO/trunk $SVN_REPO/tags/$TRAVIS_TAG -m "Take snapshot of $TRAVIS_TAG" --username $SVN_USER --password $SVN_PASS --non-interactive 2>/dev/null
+		svn copy $SVN_REPO/temp $SVN_REPO/tags/$TRAVIS_TAG -m "Take snapshot of $TRAVIS_TAG" --username $SVN_USER --password $SVN_PASS --non-interactive 2>/dev/null
 	else
 		echo "tags/$TRAVIS_TAG already exists."
 	fi
