ebb7c00b0ab0134d3051324de9c8207d7e4038eb
Marqas <marqas36@gmail.com>
2018-08-30 20:07:12 +0530

    this?

--- scripts/deploy5.sh
+++ scripts/deploy5.sh
@@ -50,15 +50,15 @@ if [ -e "bin/build.sh" ]; then
 fi
 
 cd $BASE_DIR
+echo "Ignoring GitHub specific files"
+
+svn propset svn:ignore "README.md" ./
 echo "Syncing git repository to svn"
 rsync -a --exclude=".svn" --checksum --delete ./git/ ./temp/$TRAVIS_TAG/
 rm -fr ./git
 
 cd ./temp/$TRAVIS_TAG/
 
-echo "Ignoring GitHub specific files"
-
-svn propset svn:ignore "README.md" ./
 
 if [ -e ".distignore" ]; then
 	echo "svn propset form .distignore"
