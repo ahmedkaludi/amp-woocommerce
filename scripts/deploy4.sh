178bf48cc1f8bf5e31ea5ecf53a0eabc2f0a2fa3
Marqas <marqas36@gmail.com>
2018-08-30 13:05:06 +0530

    lets go step by step

--- scripts/deploy4.sh
+++ scripts/deploy4.sh
@@ -35,62 +35,72 @@ echo "Checking out beta from $SVN_REPO ..."
 #cd $SVN_REPO 
 # svn co -q $SVN_REPO
 # mkdir trunk
-svn co -q $SVN_REPO/tags
-echo "deleting previous directory"
-rm --force $SVN_REPO/tags/beta
-mkdir beta
-svn co -q $SVN_REPO/tags/beta
-echo "Getting clone from $GH_REF to $SVN_REPO ..."
-git clone -q $GH_REF ./git
+svn co -q $SVN_REPO
+echo "create directory"
+mkdir temp
+svn commit -m "commit to create directory" --username $SVN_USER --password $SVN_PASS --non-interactive 2>/dev/null
+echo "directory created"
 
-cd ./git
 
-if [ -e "bin/build.sh" ]; then
-	echo "Starting bin/build.sh."
-	bash bin/build.sh
-fi
 
-cd $BASE_DIR
 
-echo "Syncing git repository to svn"
-rsync -a --exclude=".svn" --checksum --delete ./git/ ./beta/
-rm -fr ./git
 
-cd ./beta
 
-if [ -e ".distignore" ]; then
-	echo "svn propset form .distignore"
-	svn propset -q -R svn:ignore -F .distignore .
+# echo "deleting previous directory"
+# rm --force $SVN_REPO/tags/beta
+# mkdir beta
+# svn co -q $SVN_REPO/tags/beta
+# echo "Getting clone from $GH_REF to $SVN_REPO ..."
+# git clone -q $GH_REF ./git
 
-else
-	if [ -e ".svnignore" ]; then
-		echo "svn propset"
-		svn propset -q -R svn:ignore -F .svnignore .
-	fi
-fi
-echo "Ignoring GitHub specific files"
-# svn propset svn:ignore "README.md
-# Thumbs.db
-# .github/*
-# .git
-# .gitattributes
-# .gitignore" $SVN_REPO/tags/beta/
-echo "Run svn add"
-svn st | grep '^!' | sed -e 's/\![ ]*/svn del -q /g' | sh
-echo "Run svn del"
-svn st | grep '^?' | sed -e 's/\?[ ]*/svn add -q /g' | sh
+# cd ./git
 
-# If tag number and credentials are provided, commit to trunk.
-if [[ $TRAVIS_TAG && $SVN_USER && $SVN_PASS ]]; then
-	if [[ ! -d tags/$TRAVIS_TAG ]]; then
-		echo "Commit to $SVN_REPO."
-		svn commit -m "commit version $TRAVIS_TAG" --username $SVN_USER --password $SVN_PASS --non-interactive 2>/dev/null
-		echo "Take snapshot of $TRAVIS_TAG"
-		#svn copy $SVN_REPO/trunk $SVN_REPO/tags/$TRAVIS_TAG -m "Take snapshot of $TRAVIS_TAG" --username $SVN_USER --password $SVN_PASS --non-interactive 2>/dev/null
-	else
-		echo "tags/$TRAVIS_TAG already exists."
-	fi
-else
-	echo "Nothing to commit and check \`svn st\`."
-	svn st
-fi
+# if [ -e "bin/build.sh" ]; then
+# 	echo "Starting bin/build.sh."
+# 	bash bin/build.sh
+# fi
+
+# cd $BASE_DIR
+
+# echo "Syncing git repository to svn"
+# rsync -a --exclude=".svn" --checksum --delete ./git/ ./beta/
+# rm -fr ./git
+
+# cd ./beta
+
+# if [ -e ".distignore" ]; then
+# 	echo "svn propset form .distignore"
+# 	svn propset -q -R svn:ignore -F .distignore .
+
+# else
+# 	if [ -e ".svnignore" ]; then
+# 		echo "svn propset"
+# 		svn propset -q -R svn:ignore -F .svnignore .
+# 	fi
+# fi
+# echo "Ignoring GitHub specific files"
+# # svn propset svn:ignore "README.md
+# # Thumbs.db
+# # .github/*
+# # .git
+# # .gitattributes
+# # .gitignore" $SVN_REPO/tags/beta/
+# echo "Run svn add"
+# svn st | grep '^!' | sed -e 's/\![ ]*/svn del -q /g' | sh
+# echo "Run svn del"
+# svn st | grep '^?' | sed -e 's/\?[ ]*/svn add -q /g' | sh
+
+# # If tag number and credentials are provided, commit to trunk.
+# if [[ $TRAVIS_TAG && $SVN_USER && $SVN_PASS ]]; then
+# 	if [[ ! -d tags/$TRAVIS_TAG ]]; then
+# 		echo "Commit to $SVN_REPO."
+# 		svn commit -m "commit version $TRAVIS_TAG" --username $SVN_USER --password $SVN_PASS --non-interactive 2>/dev/null
+# 		echo "Take snapshot of $TRAVIS_TAG"
+# 		#svn copy $SVN_REPO/trunk $SVN_REPO/tags/$TRAVIS_TAG -m "Take snapshot of $TRAVIS_TAG" --username $SVN_USER --password $SVN_PASS --non-interactive 2>/dev/null
+# 	else
+# 		echo "tags/$TRAVIS_TAG already exists."
+# 	fi
+# else
+# 	echo "Nothing to commit and check \`svn st\`."
+# 	svn st
+# fi
