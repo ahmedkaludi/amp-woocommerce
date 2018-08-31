9f273f751cb5fc8546271b2334b04d69b65e8717
Marqas <marqas36@gmail.com>
2018-08-31 20:10:29 +0530

    revert

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,59 +11,79 @@ if [[ ! $SVN_REPO ]]; then
 	echo "SVN repo is not specified."
 	exit
 fi
-# Create branch name
-TAG="beta"
+TRAVIS_TAG="beta"
+SVN_USER="$WP_ORG_USERNAME"
+SVN_PASS="$WP_ORG_PASSWORD"
 # Untrailing slash of SVN_REPO path
 SVN_REPO=`echo $SVN_REPO | sed -e "s/\/$//"`
 # Git repository
 GH_REF=https://github.com/${TRAVIS_REPO_SLUG}.git
-# make build directory
+echo "$TRAVIS_REPO_SLUG"
+
+#svn co -q $SVN_REPO
+# cd amp-woocommerce
+# wget https://raw.githubusercontent.com/miya0001/wp-svnignore/master/.svnignore
+# svn propset -R svn:ignore -F .svnignore .
+# cd ..
+echo "Starting deploy..."
+
 mkdir build
 
 cd build
 BASE_DIR=$(pwd)
 
-# Checking out temp from $SVN_REPO ...
+echo "Checking out temp from $SVN_REPO ..."
 svn co -q $SVN_REPO/temp
-
-# create $TAG directory
-mkdir $TAG
-# make a temporary git folder
+# echo "delete current temp/beta"
+# rm -fr ./beta
+# echo "create new beta in temp"
+echo "create $TRAVIS_TAG directory"
+mkdir $TRAVIS_TAG
+echo "Getting clone from $GH_REF to $SVN_REPO ..."
 mkdir git
 cd ./git
-# Getting clone from $GH_REF to $SVN_REPO ...
 git clone -q $GH_REF --branch beta
-
+#git branch -a
+#git checkout beta
+ls
 cd $BASE_DIR
-
-# Syncing git repository to svn
-rsync -a --exclude=".svn" --checksum --delete ./git/amp-woocommerce/ ./temp/$TAG/
+ls
+echo "Syncing git repository to svn"
+rsync -a --exclude=".svn" --checksum --delete ./git/amp-woocommerce/ ./temp/$TRAVIS_TAG/
 rm -fr ./git
 
-cd ./temp/$TAG/
+cd ./temp/$TRAVIS_TAG/
 
-# Run svn add
+echo "Run svn add"
 svn st | grep '^?' | sed -e 's/\?[ ]*/svn add -q /g' | sh
-# Run svn del
+echo "Run svn del"
 svn st | grep '^!' | sed -e 's/\![ ]*/svn del -q /g' | sh
 
+ls 
 svn delete --force .git scripts .travis.yml
-
+#svn delete --force $SVN_REPO/temp/$TRAVIS_TAG/.git -m "deleting .git folder"
+ls
 # If tag number and credentials are provided, commit to temp.
-if [[ $TAG && $WP_ORG_USERNAME && $WP_ORG_PASSWORD ]]; then
-    # "Commit to $SVN_REPO."
-    svn commit -m "commit version $TAG" --username $WP_ORG_USERNAME --password $WP_ORG_PASSWORD --non-interactive 2>/dev/null
-    # "Take snapshot of $TAG"
-    # checkout svn repo and delete $Tag directory if its already present
-    svn co $SVN_REPO
-    if [[ -d $SVN_REPO/tags/$TAG ]]; then
-        echo "$TAG exists"
-    else
-        echo "$TAG doesnt exists"
+if [[ $TRAVIS_TAG && $SVN_USER && $SVN_PASS ]]; then
+	if [[ -d $SVN_REPO/tags/$TRAVIS_TAG ]]; then
+		echo "delete existing beta"
     fi
-    echo "move temp/$TAG into tags/$TAG"
-	svn delete $SVN_REPO/tags/$TAG -m "deleting existing beta" --username $WP_ORG_USERNAME --password $WP_ORG_PASSWORD --force --non-interactive 2>/dev/null
-	svn move $SVN_REPO/temp/$TAG $SVN_REPO/tags/$TAG -m "Move from temp/beta to tags/beta" --username $WP_ORG_USERNAME --password $WP_ORG_PASSWORD --force --non-interactive 2>/dev/null
+    if [[ ! -d $SVN_REPO/tags/$TRAVIS_TAG ]]; then
+        echo "Commit to $SVN_REPO."
+        svn commit -m "commit version $TRAVIS_TAG" --username $SVN_USER --password $SVN_PASS --non-interactive 2>/dev/null
+        echo "Take snapshot of $TRAVIS_TAG"
+        svn co $SVN_REPO
+        if [[ -d ./tags/$TRAVIS_TAG ]]; then
+            echo "$TRAVIS_TAG exists"
+        else
+            echo "$TRAVIS_TAG doesnt exists"
+        fi
+        echo "move temp/$TRAVIS_TAG into tags/$TRAVIS_TAG"
+		svn delete $SVN_REPO/tags/$TRAVIS_TAG -m "deleting existing beta" --username $SVN_USER --password $SVN_PASS --force --non-interactive 2>/dev/null
+		svn move $SVN_REPO/temp/$TRAVIS_TAG $SVN_REPO/tags/$TRAVIS_TAG -m "Move from temp/beta to tags/beta" --username $SVN_USER --password $SVN_PASS --force --non-interactive 2>/dev/null
+	else
+		echo "tags/$TRAVIS_TAG already exists."
+	fi
 else
 	echo "Nothing to commit and check \`svn st\`."
 	svn st
