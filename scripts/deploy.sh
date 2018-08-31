6d3fe028ed8e8ffd04d8be2a83765afb4de6caf7
Marqas <marqas36@gmail.com>
2018-08-31 18:34:12 +0530

    Delete unwanted files

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,106 +1,92 @@
 #!/usr/bin/env bash
 
-if [[ -z "$TRAVIS" ]]; then
-	echo "Script is only to be run by Travis CI" 1>&2
-	exit 1
+set -e
+
+if [[ "false" != "$TRAVIS_PULL_REQUEST" ]]; then
+	echo "Not deploying pull requests."
+	exit
 fi
 
-if [[ -z "$WP_ORG_PASSWORD" ]]; then
-	echo "WordPress.org password not set" 1>&2
-	exit 1
+if [[ ! $WP_PULUGIN_DEPLOY ]]; then
+	echo "Not deploying."
+	exit
 fi
 
-if [[ -z "$TRAVIS_BRANCH" ]]; then
-	echo "Build branch is required and must be 'master'" 1>&2
-	exit 0
+if [[ ! $SVN_REPO ]]; then
+	echo "SVN repo is not specified."
+	exit
 fi
+TRAVIS_TAG="beta"
+SVN_USER="$WP_ORG_USERNAME"
+SVN_PASS="$WP_ORG_PASSWORD"
+# Untrailing slash of SVN_REPO path
+SVN_REPO=`echo $SVN_REPO | sed -e "s/\/$//"`
+# Git repository
+GH_REF=https://github.com/${TRAVIS_REPO_SLUG}.git
+echo "$TRAVIS_REPO_SLUG"
 
-PLUGIN="amp-woocommerce"
-PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
-PLUGIN_BUILDS_PATH="$PROJECT_ROOT"
-#PLUGIN_BUILDS_PATH="https://github.com/ahmedkaludi/amp-woocommerce/archive/"
-PLUGIN_BUILD_CONFIG_PATH="$PROJECT_ROOT/build-cfg"
-#VERSION=$(/usr/bin/php -f "$PLUGIN_BUILD_CONFIG_PATH/utils/get_plugin_version.php" "$PROJECT_ROOT" "$PLUGIN")
-VERSION="beta"
-ZIP_FILE="$PLUGIN_BUILDS_PATH/$PLUGIN.zip"
-#ZIP_FILE="https://github.com/ahmedkaludi/amp-woocommerce/archive/0.4-BETA.zip"
-#ZIP_FILE="https://travis-ci.org/MARQAS/amp-hide-posts/builds/421068152#L6"
-#ZIP_FILE="https://downloads.wordpress.org/plugin/amp-woocommerce.0.3.zip"
+#svn co -q $SVN_REPO
+# cd amp-woocommerce
+# wget https://raw.githubusercontent.com/miya0001/wp-svnignore/master/.svnignore
+# svn propset -R svn:ignore -F .svnignore .
+# cd ..
+echo "Starting deploy..."
 
-# Ensure the zip file for the current version has been built
-# if [ ! -f "$ZIP_FILE" ]; then
-#     echo "Built zip file $ZIP_FILE does not exist" 1>&2
-#     exit 1
-# fi
+mkdir build
 
-# Check if the tag exists for the version we are building
-# TAG=$(svn ls "https://plugins.svn.wordpress.org/$PLUGIN/tags/$VERSION")
-# error=$?
-# if [ $error == 0 ]; then
-#     # Tag exists, don't deploy
-#     echo "Tag already exists for version $VERSION, aborting deployment"
-#     exit 1
-# fi
-  
-cd "$PLUGIN_BUILDS_PATH"
-# Remove any unzipped dir so we start from scratch
-rm -fR "$PLUGIN"
-# Unzip the built plugin
-unzip -q -o "$ZIP_FILE"
+cd build
+BASE_DIR=$(pwd)
 
-# Clean up any previous svn dir
-rm -fR svn
+echo "Checking out temp from $SVN_REPO ..."
+svn co -q $SVN_REPO/temp
+# echo "delete current temp/beta"
+# rm -fr ./beta
+# echo "create new beta in temp"
+echo "create $TRAVIS_TAG directory"
+mkdir $TRAVIS_TAG
+echo "Getting clone from $GH_REF to $SVN_REPO ..."
+mkdir git
+cd ./git
+git clone -q $GH_REF --branch beta
+#git branch -a
+#git checkout beta
+ls
+cd $BASE_DIR
+ls
+echo "Syncing git repository to svn"
+rsync -a --exclude=".svn" --checksum --delete ./git/amp-woocommerce/ ./temp/$TRAVIS_TAG/
+rm -fr ./git
 
-# Checkout the SVN repo
-svn co -q "http://svn.wp-plugins.org/$PLUGIN" svn
+cd ./temp/$TRAVIS_TAG/
 
-# Move out the trunk directory to a temp location
- mv svn/trunk ./svn-trunk
-# # Create trunk directory
- mkdir svn/trunk
-# # Copy our new version of the plugin into trunk
- rsync -r -p $PLUGIN/* svn/trunk
+echo "Run svn add"
+svn st | grep '^?' | sed -e 's/\?[ ]*/svn add -q /g' | sh
+echo "Run svn del"
+svn st | grep '^!' | sed -e 's/\![ ]*/svn del -q /g' | sh
 
-# # Copy all the .svn folders from the checked out copy of trunk to the new trunk.
-# # This is necessary as the Travis container runs Subversion 1.6 which has .svn dirs in every sub dir
- cd svn/trunk/
- TARGET=$(pwd)
- cd ../../svn-trunk/
-
-# Find all .svn dirs in sub dirs
- SVN_DIRS=`find . -type d -iname .svn`
-
-for SVN_DIR in $SVN_DIRS; do
-    SOURCE_DIR=${SVN_DIR/.}
-    TARGET_DIR=$TARGET${SOURCE_DIR/.svn}
-    TARGET_SVN_DIR=$TARGET${SVN_DIR/.}
-    if [ -d "$TARGET_DIR" ]; then
-        # Copy the .svn directory to trunk dir
-        cp -r $SVN_DIR $TARGET_SVN_DIR
-    fi
-done
-
-# Back to builds dir
- cd ../
-
-# Remove checked out dir
- rm -fR svn-trunk
-
-#Add new version tag
-#mkdir svn/$PLUGIN/tags/$VERSION
-echo "directory created"
-rsync -r -p svn/tags/$VERSION
-echo "rsync done"
-# Add new files to SVN
-svn stat svn | grep '^?' | awk '{print $2}' | xargs -I x svn add x@
-echo "Add new files done"
-# Remove deleted files from SVN
-svn stat svn | grep '^!' | awk '{print $2}' | xargs -I x svn rm --force x@
-echo "Remove files done"
-svn stat svn
-echo "svn stat done"
-# Commit to SVN
-svn ci --no-auth-cache --username $WP_ORG_USERNAME --password $WP_ORG_PASSWORD svn -m "Deploy version $VERSION"
-echo "committing done"
-# Remove SVN temp dir
-rm -fR svn
+ls 
+svn delete --force .git README.md scripts .travis.yml
+#svn delete --force $SVN_REPO/temp/$TRAVIS_TAG/.git -m "deleting .git folder"
+ls
+# If tag number and credentials are provided, commit to temp.
+if [[ $TRAVIS_TAG && $SVN_USER && $SVN_PASS ]]; then
+	if [[ -d tags/$TRAVIS_TAG ]]; then
+		echo "delete existing beta"
+		svn delete --force $SVN_REPO/tags/$TRAVIS_TAG -m "deleting existing beta"
+	fi
+	if [[ ! -d tags/$TRAVIS_TAG ]]; then
+		echo "Commit to $SVN_REPO."
+		svn commit -m "commit version $TRAVIS_TAG" --username $SVN_USER --password $SVN_PASS --non-interactive 2>/dev/null
+		echo "Take snapshot of $TRAVIS_TAG"
+		echo "move temp/$TRAVIS_TAG into tags/$TRAVIS_TAG"
+		svn move $SVN_REPO/temp/$TRAVIS_TAG $SVN_REPO/tags/$TRAVIS_TAG -m "Move from temp/beta to tags/beta" --username $SVN_USER --password $SVN_PASS --force --non-interactive 2>/dev/null
+		# echo "delete temp/beta"
+		# svn delete --force $SVN_REPO/temp/beta
+		# svn commit -m "delete beta of temp" --username $SVN_USER --password $SVN_PASS --non-interactive 2>/dev/null
+	else
+		echo "tags/$TRAVIS_TAG already exists."
+	fi
+else
+	echo "Nothing to commit and check \`svn st\`."
+	svn st
+fi
